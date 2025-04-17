295ffd9c78b81f1c53b89250329c21b095bb7882
Isabella <isabellaotoo25@gmail.com>
2025-04-17 22:39:37 +0000

    Generate controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'notes/index'
+  get 'notes/show'
+  get 'notes/new'
+  get 'notes/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
